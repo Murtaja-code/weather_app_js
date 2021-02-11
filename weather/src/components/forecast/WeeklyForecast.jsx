dd38c33ff7e82d348ef860fa162ac8b2882963de
klay964 <dody200039@gmail.com>
2021-02-12 00:34:07 +0300

    Refactor:table improvment fixes #23
    move map function to the right container

---
+++ weather/src/components/forecast/WeeklyForecast.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Table } from 'react-bootstrap';
+
+function WeeklyForecast({ forecastWeather }) {
+  return (
+    <div>
+      <h3> A weekly Forecast</h3>
+      <Container>
+        <Table striped hover bordered responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Day</th>
+              <th>Low</th>
+              <th>High</th>
+              <th>Wind</th>
+            </tr>
+          </thead>
+          {forecastWeather}
+        </Table>
+      </Container>
+    </div>
+  );
+}
+
+export default WeeklyForecast;
